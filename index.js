29175f91cca05068e9fa3bebfa3fabb3feb0124b
Prajay Tipre <prajaytipre@gmail.com>
2021-11-03 13:43:20 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -3,33 +3,33 @@
 const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
-    cats.push(name);
+    cats.push(name)
 }
 
 function destructivelyPrependCat(name) {
-    cats.unshift(name);
+    cats.unshift(name)
 }
 
 function destructivelyRemoveLastCat() {
-    cats.pop();
+    cats.pop()
 }
 
 function destructivelyRemoveFirstCat() {
-    cats.shift();
+    cats.shift()
 }
 
 function appendCat(name) {
-    return [...cats, name];
+    return [...cats, name]
 }
 
 function prependCat(name) {
-    return [name, ...cats];
+    return [name, ...cats]
 }
 
 function removeLastCat() {
-    return cats.slice(0, cats.length - 1);
+    return cats.slice(0, cats.length - 1)
 }
 
 function removeFirstCat() {
-    return cats.slice(1);
+    return cats.slice(1, cats.length)
 }
